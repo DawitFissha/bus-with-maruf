c99918f658c39786f927151959fc474ec2b8f446
Dawit Fissha <dawitfissha1@gmail.com>
2022-12-25 15:06:24 +0300

    corrections on seat picker component

--- src/Components/seat-picker/seatInfoBox.tsx
+++ src/Components/seat-picker/seatInfoBox.tsx
@@ -1,34 +1,76 @@
 import * as React from 'react'
-import SeatInfo from './seatInfo'
 import SeatStausBox from './seatStatusBox'
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import GlobalStyles from '@mui/material/GlobalStyles';
+import TaskIcon from '@mui/icons-material/Task';
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
 interface InfoBoxProps {
     total:number
     availabel:number
     plateNo:string
+    completeSeatChoosing:()=>void
   }
+
+const H5 = (props:{children:React.ReactNode})=>{
+    return(
+        <>
+        <GlobalStyles styles={{ h5: { 
+
+fontFamily:" -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol",
+fontSize: '1rem',
+lineHeight: 1.5,
+letterSpacing: '0.1px',
+color: "rgb(33, 33, 33)",
+fontWeight: 700,
+// scroll-margin-top: calc(var(--MuiDocs-header-height) + 32px);
+
+        } }} />
+      <h5>
+        {props.children}
+      </h5>
+        </>
+    )
+}
 export default function InfoBox(props:InfoBoxProps) {
-    const {total,availabel,plateNo} = props
+    const {total,availabel,plateNo,completeSeatChoosing} = props
     return (
-        <Box
-        sx={{
-            display:'flex',
-            flexDirection:'column',
-            width:'100%',
-            border:'1px solid black',
-            color:'white',
-            m:.6
-        }}
+        <>
+        <Grid
+        sx={{ml:1}}
+        container
         >
-         <SeatInfo total={total} available={availabel} plateNo={plateNo}/>
-        
-            <div
-            style={{display:'flex',columnGap:'30px',marginLeft:'15px'}}
-            >
-                <SeatStausBox status="Occupied"/>
-                <SeatStausBox status="Free"/> 
-                <SeatStausBox status="Selected"/>           
-            </div>
-        </Box>
+            <Grid item md={4} xs={12}>
+            <H5>Total Seats - <strong>{total}</strong></H5>
+            <SeatStausBox status="Occupied"/>
+
+            </Grid>
+            <Grid item md={4} xs={12}>
+            <H5>Available Seats - <strong>{availabel}</strong></H5>
+            <SeatStausBox status="Free"/> 
+            </Grid>
+            <Grid item md={4} xs={12}>
+            <H5>PlateNo - <strong>{plateNo}</strong></H5>
+            <SeatStausBox status="Selected"/>       
+            </Grid>
+        </Grid>
+        <Box sx={{display:'flex',alignItems:'end',mb:1,ml:1}}>
+            <Box sx={{flexGrow:1}}>
+            <Typography sx={{color:'orange'}}>Only maximum of two seats can be selected </Typography>
+            </Box>
+               <Tooltip title="Done">
+               <IconButton 
+                onClick = {()=>completeSeatChoosing()}
+                >
+                    <TaskIcon color='primary' fontSize='large'/>
+                </IconButton>
+                
+               </Tooltip>
+            
+                </Box>
+        </>
     )
     }
