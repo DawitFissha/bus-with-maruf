8b293204d2e7e3e7d5f86c91897612bb6752c0c0
Dawit Fissha <dawitfissha1@gmail.com>
2022-12-25 13:09:15 +0300

    type checking is removed from dragabble

--- src/Components/dialog/paperComponent.tsx
+++ src/Components/dialog/paperComponent.tsx
@@ -1,12 +1,13 @@
 import Paper, { PaperProps } from '@mui/material/Paper';
 import Draggable from 'react-draggable';
+const Draggable1:any = Draggable 
 export default function PaperComponent(props: PaperProps) {
     return (
-      <Draggable
+      <Draggable1
         handle="#draggable-dialog-title"
         cancel={'[class*="MuiDialogContent-root"]'}
       >
         <Paper {...props} />
-      </Draggable>
+      </Draggable1>
     );
   }
