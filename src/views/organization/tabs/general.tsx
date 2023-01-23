53911afa299dcb49bd6f351e4ab44be2d2d8d1f6
Dawit Fissha <dawitfissha1@gmail.com>
2023-01-23 16:10:40 +0300

    orgn/general updated

--- src/views/organization/tabs/general.tsx
+++ src/views/organization/tabs/general.tsx
@@ -2,45 +2,97 @@ import * as React from 'react'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import {FormWrapper} from '../../../Components/formWrapper'
-import {Button } from '@mui/material'
-
+import {useUpdateOrganizationMutation,useGetLoggedInOrganizationQuery} from '../../../store/bus_api'
 import { useFormik } from 'formik';
+import Button from '@mui/material/Button'
+import useError from '../../../utils/hooks/useError'
+import Alert from '@mui/material/Alert'
+import {SavingProgress} from '../../../Components/common-registration-form/savingProgress'
 type ORGN =  {
-    name:string;
-    nameInAmharic:string;
-    Tin:string;
+    OrganizationName:string;
+    OrganizationNameAmharic:string;
+    TinNumber:string;
     motto?:string
 }
 const validate = (values:ORGN) => {
     const errors:Partial<ORGN> = {}
-    if (!values.name) {
-      errors.name = 'This field is required';
+    if (!values.OrganizationName) {
+      errors.OrganizationName = 'This field is required';
     } 
-     if(!values.nameInAmharic){
-      errors.nameInAmharic = 'This field is required';
+     if(!values.OrganizationNameAmharic){
+      errors.OrganizationNameAmharic = 'This field is required';
     }
-    if(!values.Tin){
-      errors.Tin = 'This field is required';
+    if(!values.TinNumber){
+      errors.TinNumber = 'This field is required';
     }
 
     return errors;
   };
 const GeneralOrgnInfo = (props:{organizationInfo:any})=>{
 const {organizationInfo} = props;
+// from redux
+const {data}=useGetLoggedInOrganizationQuery()
+const loggedInOrganizationId = data?._id
+const organizationLogo = data?._logo
+const [updateOrganizationInfo,{isLoading:isUpdating}]=useUpdateOrganizationMutation()
+//local states
+const [image,setImage] = React.useState<File|Blob>()
+const [url,setUrl] = React.useState('')
+const [UpdatingErrorOccured,updatingErrorMessage,setUpdatingErrorOccured,setUpdatingErrorMessage] = useError()
 //formik
 const formik = useFormik({
     initialValues:{
-        name:organizationInfo?.organizationName,
-        nameInAmharic:organizationInfo?.organizationNameAmharic,
-        Tin:organizationInfo?.tin,
+        OrganizationName:organizationInfo?.organizationName,
+        OrganizationNameAmharic:organizationInfo?.organizationNameAmharic,
+        TinNumber:organizationInfo?.tin,
         motto:'',
     },
     validate,
-    onSubmit:()=>{
+    onSubmit:async (values,{resetForm})=>{
         console.log('submited')
+        if(Boolean(image)){
+            const data = new FormData()
+            data.append("file", image as Blob)
+            data.append("upload_preset", "jmtirbf1")
+            data.append("cloud_name","diz9yj1qj")
+            fetch("  https://api.cloudinary.com/v1_1/diz9yj1qj/image/upload",
+            {method:"post",body: data})
+            .then(resp => resp.json())
+            .then(data => {setUrl(data.url)})
+            .catch(err => console.log(err))
+        }
+        try {
+            await updateOrganizationInfo({
+              loggedInOrganizationId,
+              organizationName:values.OrganizationName,
+              organizationNameAmharic:values.OrganizationNameAmharic,
+              tin:values.TinNumber,
+              logo:Boolean(url)?url:organizationLogo,
+              
+            }).unwrap()
+            resetForm({values:{
+              OrganizationName: "",
+              OrganizationNameAmharic:"",
+              TinNumber:"",
+
+            }})
+            setUpdatingErrorOccured(false)
+          }
+          
+          catch(err) {
+            setUpdatingErrorOccured(true)
+            setUpdatingErrorMessage(`Failed to Update Organization , ${err.data.message}`)
+
+          }
+        
+
     }
 })
+
+
     return (
+   <>
+   <SavingProgress loading={isUpdating}/>
    
              <Box
         sx={{
@@ -50,13 +102,13 @@ const formik = useFormik({
         }}
         >
         
-            <form>
+            <form onSubmit={formik.handleSubmit}>
             <FormWrapper>
             <TextField
             id="OrganizationName"
             name="OrganizationName"
             label="Organization Name"
-            value={formik.values.name}
+            value={formik.values.OrganizationName}
             onChange={formik.handleChange}
       />
             </FormWrapper>
@@ -66,7 +118,7 @@ const formik = useFormik({
         id="OrganizationNameAmharic"
         name="OrganizationNameAmharic"
         label="የ ድርጅት ስም"
-        value={formik.values.nameInAmharic}
+        value={formik.values.OrganizationNameAmharic}
         onChange={formik.handleChange}
       />
             </FormWrapper>
@@ -75,7 +127,7 @@ const formik = useFormik({
                 id="TinNumber"
                 name="TinNumber"
                 label="Tin Number"
-                value={formik.values.Tin}
+                value={formik.values.TinNumber}
                 onChange={formik.handleChange}
       />
             </FormWrapper>
@@ -92,14 +144,32 @@ const formik = useFormik({
                     <FormWrapper>
                         <Box sx={{display:'flex',alignItems:'center',gap:'5px'}}>
                             <p>Attach Company Logo</p>
-                            <Button
-                            sx={{fontSize:'14px',width:'100px',borderRadius:0}}
-                            >Browse</Button>
+                            <input
+                            onChange={
+                                (e)=>{
+                                    setImage(e.target?.files[0])
+                                }
+                            }
+                            type={'file'}
+                            style={{marginLeft:10}}
+                            />
                             </Box>
                     </FormWrapper>
-
+                <FormWrapper>
+                    <Button type="submit">
+                        Save
+                    </Button>
+                </FormWrapper>
             </form>
+            {
+      UpdatingErrorOccured && (<FormWrapper>
+            <Alert severity="error">
+            <strong>{updatingErrorMessage}</strong>
+            </Alert>
+            </FormWrapper>)
+            }
         </Box> 
+        </>
     )
 }
 export default GeneralOrgnInfo
